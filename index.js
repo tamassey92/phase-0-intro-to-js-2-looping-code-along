0b5ffc250b03abefbe210f1432ea3d12419cfd71
Taylor Massey <tamassey92@gmail.com>
2022-04-10 16:30:51 -0600

    commit tree 8f241fad36dd049f6ec3ef5a8015fd6b4bd92e91

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solutions in this file
+const writeCards=(names,message)=>{
+    let messages = [];
+        for(let name in names){
+            messages.push(`Thank you, ${names[name]}, for the wonderful ${message} gift!`);
+        }
+    return messages;
+}
